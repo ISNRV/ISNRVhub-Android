e6e54ad70ca4e54ccf9d134484ffee1441f10d7b
Amr Abed <amraabed@gmail.com>
2018-12-11 11:09:54 +0200

    Migrate to AndroidX

--- app/src/main/java/com/isnrv/Main.java
+++ app/src/main/java/com/isnrv/Main.java
@@ -2,7 +2,7 @@ package com.isnrv;
 
 import android.app.ActionBar;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 
 /**
  * Main Activity
